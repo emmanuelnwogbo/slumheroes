7d134a326906b1349712ef259ab45e7e61cdb98b
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2022-04-20 23:50:49 +0100

    add slide for desktop

--- minting-dapp/src/scripts/react/Mission.tsx
+++ minting-dapp/src/scripts/react/Mission.tsx
@@ -5,11 +5,11 @@ interface Props {
 }
 
 interface State {
-
+  currentThing: number
 }
 
 const defaultState: State = {
-  
+  currentThing: 1
 };
 
 export default class Mission extends React.Component<Props, State> {
@@ -19,13 +19,31 @@ export default class Mission extends React.Component<Props, State> {
     this.state = defaultState;
   }
 
+  private nextSlide(): void {
+    if (this.state.currentThing === 4) {
+      let next = 1;
+      this.setState({
+        currentThing: next
+      });
+
+      return
+    }
+
+    let next = this.state.currentThing;
+    next+=1;
+    this.setState({
+      currentThing: next
+    });
+  }
+
   render() {
     return (
       <>
         <div className="mission">
           <h2 className="mission__h2">Mission/Roadmap</h2>
           <div className="mission__slides">
-            <div className="mission__slide">
+            <div className={
+              this.state.currentThing === 1 ? 'currentthing mission__slide' : this.state.currentThing > 2 ? 'notcurrentthing mission__slide' : 'previousthing mission__slide'}>
               <div className="mission__slideleft">
                 <div className="mission__slideleft">
                   <h2>Phase 1</h2>
@@ -40,11 +58,56 @@ export default class Mission extends React.Component<Props, State> {
                 </figure>
               </div>
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className={
+              this.state.currentThing === 2 ? 'currentthing mission__slide' : this.state.currentThing > 2 ? 'previousthing mission__slide' : 'notcurrentthing mission__slide' }>
+              <div className="mission__slideleft">
+                <div className="mission__slideleft">
+                  <h2>Phase 2</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat hendrerit a maximus augue porttitor.
+                  </p>
+                </div>
+              </div>
+              <div className="mission__slideright">
+                <figure>
+                  <img src="/build/images/unsplash.png"/>
+                </figure>
+              </div>
+            </div>
+            <div className={
+              this.state.currentThing === 3 ? 'currentthing mission__slide' : this.state.currentThing > 3 ? 'previousthing mission__slide' : 'notcurrentthing mission__slide' }>
+              <div className="mission__slideleft">
+                <div className="mission__slideleft">
+                  <h2>Phase 3</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat hendrerit a maximus augue porttitor.
+                  </p>
+                </div>
+              </div>
+              <div className="mission__slideright">
+                <figure>
+                  <img src="/build/images/unsplash.png"/>
+                </figure>
+              </div>
+            </div>
+            <div className={
+              this.state.currentThing === 4 ? 'currentthing mission__slide' : this.state.currentThing === 1 ? 'previousthing mission__slide' : 'notcurrentthing mission__slide' }>
+              <div className="mission__slideleft">
+                <div className="mission__slideleft">
+                  <h2>Phase 4</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat hendrerit a maximus augue porttitor.
+                  </p>
+                </div>
+              </div>
+              <div className="mission__slideright">
+                <figure>
+                  <img src="/build/images/unsplash.png"/>
+                </figure>
+              </div>
+            </div>
           </div>
-          <div className="mission__slidearrow">
+          <div className="mission__slidearrow" onClick={() => this.nextSlide()}>
             <figure>
               <img src="/build/images/nextslide.svg"/>
             </figure>
